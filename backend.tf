e3fcbac0c81a162ce76ed0ac41f9d11c2ecf3252
gustavohaj <gustavohenriqueaj@gmail.com>
2025-08-24 10:34:47 -0300

    terraform cloud

---
+++ backend.tf
@@ -0,0 +1,7 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "gustavo-fiap-aula-terraform"
+#     key    = "aula08/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
---
+++ data.tf
@@ -0,0 +1,13 @@
+data "aws_iam_user" "principal_user" {
+  user_name = "ghaj"
+}
+
+data "aws_eks_cluster" "cluster"{
+  name = aws_eks_cluster.cluster.name
+}
+
+data "aws_eks_cluster_auth" "auth"{
+  name = aws_eks_cluster.cluster.name
+}
+
+
